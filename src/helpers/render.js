3a4a3f174e9fe255efd0738627c8a86e51207b0f
Mark Perkins <mark@allmarkedup.com>
2017-01-26 17:49:28 +0000

    Fix for context refs in nested data

--- src/helpers/render.js
+++ src/helpers/render.js
@@ -29,12 +29,14 @@ module.exports = function(fractal){
         } else if (merge) {
             context = _.defaultsDeep(context, defaultContext);
         }
+        
+        return source.resolve(context).then(context => {
+            // fix env for rendered components
+            let env = JSON.parse(JSON.stringify(root._env));
+            _.set(context, '_env', env);
 
-        // fix env for rendered components
-        let env = JSON.parse(JSON.stringify(root._env));
-        _.set(context, '_env', env);
-
-        return entity.render(context).then(html => new Handlebars.SafeString(html));
+            return entity.render(context).then(html => new Handlebars.SafeString(html));
+        });
     };
 
 };
